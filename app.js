93bdcb798535fa472c54a8f7ace59d9405558220
Harry <harry811016@gmail.com>
2020-07-02 20:44:28 +0800

    Install Imgur api and update admin POST and PUT

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
