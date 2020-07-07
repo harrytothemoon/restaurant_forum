69d985e067410770aaa1c08a0da801c728ad7d97
Harry <harry811016@gmail.com>
2020-07-07 23:45:20 +0800

    edit Profile function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const User = db.User
 
 const userController = {
@@ -52,6 +54,43 @@ const userController = {
         user: user.toJSON()
       })
     })
+  },
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      console.log(req.body)
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : restaurant.image,
+            })
+              .then((user) => {
+                req.flash('success_messages', 'user was successfully to update')
+                res.redirect('/users/:id')
+              })
+          })
+      })
+    }
+    else
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name,
+            image: user.image,
+          })
+            .then((user) => {
+              req.flash('success_messages', 'user was successfully to update')
+              res.redirect('/users/:id')
+            })
+        })
   },
 }
 
