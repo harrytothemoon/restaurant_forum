15a50c8f58b2ccf6179ddaa46184c6621bb4d08a
Harry <harry811016@gmail.com>
2020-07-07 14:46:39 +0800

    Add filter on category navbar & Add categories navbar on restaurants index & Show active item on category navbars

--- controllers/restController.js
+++ controllers/restController.js
@@ -3,15 +3,29 @@ const Restaurant = db.Restaurant
 const Category = db.Category
 let restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)  //query後為字串
+      whereQuery['CategoryId'] = categoryId
+    }
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
         categoryName: r.Category.name
       }))
-      return res.render('restaurants', {
-        restaurants: data
+      Category.findAll({
+        raw: true,
+        nest: true
+      }).then(categories => {
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId
+        })
       })
+
     })
   },
   getRestaurant: (req, res) => {
