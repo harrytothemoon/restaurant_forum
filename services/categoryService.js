3aaebdecd71a027857563d575267468b5a40df0b
Harry <harry811016@gmail.com>
2020-07-14 16:22:12 +0800

    PUT /api/admin/categories/:id

--- services/categoryService.js
+++ services/categoryService.js
@@ -37,6 +37,19 @@ let adminController = {
         })
     }
   },
+  putCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" })
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((category) => {
+              callback({ status: 'success', message: '' })
+            })
+        })
+    }
+  },
 }
 
 module.exports = adminController
