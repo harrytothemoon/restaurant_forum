52a725066b5b7fd8a7a2cdf73f2090d8f3961775
Harry <harry811016@gmail.com>
2020-07-14 15:21:50 +0800

    POST /api/admin/restaurants

--- routes/apis.js
+++ routes/apis.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurant', adminController.getRestaurant)
-router.get('/admin/categories', categoryController.getCategories)
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+
+router.get('/admin/categories', categoryController.getCategories)
 
 
 module.exports = router
