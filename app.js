cf13ec306320da20fc3803b0dd7981010a6ab902
Harry <harry811016@gmail.com>
2020-07-15 21:04:34 +0800

    add authenticated and authenticatedAdmin

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
-const passport = require('./config/passport')
 const { helpers } = require('faker')
 
 const app = express()
@@ -14,6 +13,8 @@ const port = process.env.PORT || 3000
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+const passport = require('./config/passport')
 
 app.engine('handlebars', handlebars({
   defaultLayout: 'main',
